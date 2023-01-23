eaf8da95ee7745ad10be465ed709fe1e386396c3
Christian Winzen <cwinzen@anexia-it.com>
2023-01-23 22:29:45 +0100

    init

---
+++ KstrwbryBinanceTraderBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Kstrwbry\BinanceTrader;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class KstrwbryBinanceTraderBundle extends AbstractBundle {}
